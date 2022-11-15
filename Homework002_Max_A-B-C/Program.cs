508587e623e33437c78de43c240e3fb85e942a9b
Aleksandr <sveta-sasha-21@yandex.ru>
2022-11-15 19:51:17 +0300

    Homework 002 Max A-B-C

---
+++ Homework002_Max_A-B-C/Program.cs
@@ -0,0 +1,38 @@
+﻿//int numberA = new Random().Next(1, 10);
+//Console.Write("Число А = ");
+//Console.WriteLine(numberA);
+//int numberB = new Random().Next(1, 10);
+//Console.Write("Число B = ");
+//Console.WriteLine(numberB);
+//int numberC = new Random().Next(1, 10);
+//Console.Write("Число C = ");
+//Console.WriteLine(numberC);
+int numberA = 24;
+int numberB = 12;
+int numberC = 7;
+if (numberA > numberB)
+{
+    if (numberA > numberC)
+    {
+        Console.Write("Максимальное число = ");
+        Console.WriteLine(numberA);
+    }
+    else
+    {
+        Console.Write("Максимальное число = ");
+        Console.WriteLine(numberC);
+    }
+}
+else
+{
+    if (numberB > numberC)
+    {
+        Console.Write("Максимальное число = ");
+        Console.WriteLine(numberB);
+    }
+    else
+    {
+        Console.Write("Максимальное число = ");
+        Console.WriteLine(numberC);
+    }
+}
