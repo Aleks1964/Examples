fa16e693f860d378becc491f1796e9ddcd88980f
Aleksandr <sveta-sasha-21@yandex.ru>
2022-11-15 22:01:04 +0300

    Homework 002 Max A-B-C - differents input

--- Homework002_Max_A-B-C/Program.cs
+++ Homework002_Max_A-B-C/Program.cs
@@ -1,4 +1,10 @@
-﻿//int numberA = new Random().Next(1, 10);
+﻿// ** Задача 4
+// ** Напишите программу, которая принимает на вход три числа
+// ** и выдаёт максимальное из этих чисел.
+
+// Если мы получаем случайные числа:
+
+//int numberA = new Random().Next(1, 10);
 //Console.Write("Число А = ");
 //Console.WriteLine(numberA);
 //int numberB = new Random().Next(1, 10);
@@ -7,9 +13,22 @@
 //int numberC = new Random().Next(1, 10);
 //Console.Write("Число C = ");
 //Console.WriteLine(numberC);
-int numberA = 24;
-int numberB = 12;
-int numberC = 7;
+
+//Если мы вводим числа в программе:
+
+// int numberA = 24;
+// int numberB = 12;
+// int numberC = 7;
+
+//Если мы вводим числа в терминале:
+
+Console.Write("Введите число А = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C = ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
 if (numberA > numberB)
 {
     if (numberA > numberC)
