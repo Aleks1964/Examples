ead53973c1d9cd544fd18cf4af31c929d44a2469
Aleksandr <sveta-sasha-21@yandex.ru>
2022-11-15 19:58:24 +0300

    Homework 003 Even A

---
+++ Homework003_Even_A/Program.cs
@@ -0,0 +1,17 @@
+﻿//int numberA = new Random().Next(1, 10);
+//Console.Write("Число А = ");
+//Console.WriteLine(numberA);
+int numberA = 3
+;
+if (numberA % 2 == 0)
+{
+    Console.Write("Ура, число ");
+    Console.Write(numberA);
+    Console.Write(" чётное!");
+}
+else
+{
+    Console.Write("Увы, число ");
+    Console.Write(numberA);
+    Console.Write(" нечётное!");
+}
