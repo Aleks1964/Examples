49bb2a0a33e4ce2c8e0f6fcf9febf44bf7fa79a3
Aleksandr <sveta-sasha-21@yandex.ru>
2022-11-15 22:17:36 +0300

    Homework 004 Even from A - differents input

---
+++ Homework004_Even_from_A/Program.cs
@@ -0,0 +1,27 @@
+﻿// ** Задача 8
+// ** Напишите программу, которая на вход принимает число (N),
+// ** на выходе показывает все чётные числа от 1 до N.
+
+
+// Если мы получаем случайное число:
+
+//int numberA = new Random().Next(1, 10);
+//Console.Write("Число А = ");
+//Console.WriteLine(numberA);
+
+//Если мы вводим число в программе:
+
+//int numberA = 5;
+
+//Если мы вводим число в терминале:
+
+Console.Write("Введите число А = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while (count <= numberA)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
