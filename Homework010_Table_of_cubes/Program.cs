000d4446839183dd197d0b015d69800939d6c0d2
Aleksandr <sveta-sasha-21@yandex.ru>
2022-11-21 21:42:58 +0300

    Homework 010 Table_of cubes

---
+++ Homework010_Table_of_cubes/Program.cs
@@ -0,0 +1,13 @@
+﻿// ** Задача 23
+
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Получаем таблицу умножения от 1 до N:");
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i}**3 = {Math.Pow(i, 3)}");
+}
