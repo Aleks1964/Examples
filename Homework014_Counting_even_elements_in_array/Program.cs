221a7f2af47317b0709807cf130624a31b5bd874
Aleksandr <sveta-sasha-21@yandex.ru>
2022-11-30 23:45:53 +0300

    Homework 014 Counting even elements in array

---
+++ Homework014_Counting_even_elements_in_array/Program.cs
@@ -0,0 +1,39 @@
+﻿// ** Задача 34. **
+// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//Ввод массива из N элементов:
+//int N = 10;
+Console.Write("Введите число N (число элементов массива): ");
+int N = int.Parse(Console.ReadLine()!);
+int[] array1 = RandArray(N);
+
+//Вывод массива на экран:
+Console.WriteLine();
+Console.WriteLine($"Массив: [{String.Join(", ", array1)}]");
+Console.WriteLine($"Количество чётных чисел в массиве равно {СountArray(array1)}");
+
+// ------------ Методы ---------------
+//Метод ввода массива RandArray:
+int[] RandArray(int N)
+{
+    int[] arrayN = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arrayN[i] = new Random().Next(100, 1000);
+        //Console.WriteLine(array2[i]);
+        //Console.WriteLine($"Массив: [{arrayN[i] % 2}]");
+    }
+    return arrayN;
+}
+
+// Метод подсчёта количества чётных элементов массива:
+int СountArray(int[] arrayA)
+{
+    int count = 0;
+    // for (int i = 0; i < arrayA.Length; i++)
+    //     if (arrayA[i] % 2 == 0) count++;
+    foreach (int i in arrayA)
+       if (i % 2 == 0) count++;
+    return count;
+}
