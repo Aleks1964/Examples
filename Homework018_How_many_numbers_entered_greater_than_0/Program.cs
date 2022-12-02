d0fb3122f19a28234d4646f91cda8ccdaf1a1132
Aleksandr <sveta-sasha-21@yandex.ru>
2022-12-02 18:30:39 +0300

    Homework 018 How many numbers entered greater than 0

---
+++ Homework018_How_many_numbers_entered_greater_than_0/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+
+// [0, 7, 8, -2, 2] -> 2
+// [1, -7, 567, 89, 223] -> 4
+
+//Ввод массива из M элементов
+Console.Write($"Введите с клавиатуры несколько чисел через пробел (в конце - Enter): ");
+
+int[] n = Console.ReadLine().Split(new char[] { '.', ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(e => Convert.ToInt32(e)).ToArray();
+//Console.WriteLine(String.Join(", ", n));
+
+Console.WriteLine($"Вы ввели {ArrayNeg(n)} чисел строго больше 0");
+
+// ------------ Методы ---------------
+// Метод подсчёта отрицательных значений в массиве ArrayNeg
+int ArrayNeg(int[] arrayA)
+{
+    int sum = 0;
+    for (int i = 0; i < arrayA.Length; i++)
+    {
+        if (arrayA[i] > 0)
+        {
+//           Console.Write(arrayA[i]);
+            sum++;
+//            Console.WriteLine($", sum = {sum}");
+        }
+    }
+    return sum;
+}
