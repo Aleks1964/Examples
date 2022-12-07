8f38723fe50d63d4ae32cbfebd9b6e2e4f190f14
Aleksandr <sveta-sasha-21@yandex.ru>
2022-12-08 00:43:34 +0300

    Homework 022 Find arithmetic mean in columns of two-dimensional array

---
+++ Homework022_Find_arithmetic_mean_in_columns_of_two-dimensional_array/Program.cs
@@ -0,0 +1,74 @@
+﻿// ** Задача 52 **
+// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// (с индексами (0,0); (1;1) и т.д.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+Console.WriteLine("Введите минимальное значение элементов массива: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное значение элементов массива: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+//Console.WriteLine("===================");
+Console.WriteLine($"Cреднее арифметическое элементов в каждом столбце равно:");
+GetAvgtColumn(array);
+//PrintArray(array);
+
+// ------------------Методы-----------------------
+
+// Метод создания двумерного массива
+int[,] GetArray(int m, int n, int minM, int maxN)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minM, maxN + 1);
+        }
+    }
+    return result;
+}
+
+// Метод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод нахождения среднего арифметического элементов в каждом столбце
+void GetAvgtColumn(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        Console.WriteLine($"Cтолбец {j}: {sum} / {array.GetLength(0)} = {(sum / array.GetLength(0)):f2} ");
+    }
+}
