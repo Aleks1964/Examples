a0eece742ac5ae174494fe137bb5128e9793f8bf
Aleksandr <sveta-sasha-21@yandex.ru>
2022-12-13 22:23:12 +0300

    Homework 029 Print sum of numbers from M to N

---
+++ Homework029_Print_sum_of_numbers_from_M_to_N/Program.cs
@@ -0,0 +1,29 @@
+﻿// ** Задача 66 **
+// Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+start:
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+if (M > N)
+{
+    Console.WriteLine("Извините, число М должно быть меньше числа N.");
+    Console.WriteLine("Повторите ввод чисел!");
+    goto start;
+}
+
+Console.Write($"Сумма натуральных чисел от {M} до {N} равна {SumOfNumbers(M, N)}");
+Console.WriteLine();
+
+int SumOfNumbers(int start, int end)
+{
+    if (start == end) return start;
+    return (start + SumOfNumbers(start + 1, end));
+}
